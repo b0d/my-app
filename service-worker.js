0a856c31b21380e6a181b86a68d1aa3e8f63901d
b0d <12471065+b0d@users.noreply.github.com>
2021-06-19 16:48:22 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8625469bae89aa71801eb550350bf809.js"
+  "./precache-manifest.8d57aa970c12372fac649472bc7bec00.js"
 );
 
 workbox.clientsClaim();
